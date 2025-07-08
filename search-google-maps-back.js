b7a8e9d4ed9405fd07e2778267ce3393850de284
Justus <15849513+JustAmply@users.noreply.github.com>
2025-07-08 10:32:19 +0200

    Update Version

--- search-google-maps-back.js
+++ search-google-maps-back.js
@@ -2,7 +2,7 @@
 // @name         Search Google Maps Back
 // @name:fr      Retour de Google Maps Dans Recherche
 // @namespace    http://tampermonkey.net/
-// @version      202410281
+// @version      2025107081
 // @description  This script bring google maps button back, makes search maps (big/mini and micro one) clickable and adds Open in Maps button back. It might not work anymore in a few months and need an update so feel free to update it whenever you want, I'll try to do it whenever I can.
 // @description:fr Ce script remet le bouton google maps, rend les cartes de recherche (grandes/mini et micro) cliquables et ajoute le bouton Open in Maps (Ouvrir sur Maps) aux grandes cartes. Il se peut qu'il ne fonctionne plus dans quelques mois et qu'une mise à jour soit nécessaire, alors n'hésitez pas à le mettre à jour quand vous le souhaitez, pour ma part je le ferai dès que possible.
 // @author       Mimouy | Mimo (Mohamed) Bouyakhlef : https://github.com/mimouy
